1c5948c47d9c4697a2cff85510532f7f0180bd83
Antonio Soliman <soliman.antonio@iisgalvanimi.edu.it>
2024-11-08 10:50:23 +0000

    inserire nuovi dati

--- main4.py
+++ main4.py
@@ -36,5 +36,37 @@ def get_data(filtro=None, valore=None, valore_min=None, valore_max=None):
             cursor.close()
             conn.close()
 
+
+@app.route('/dati/crea', methods=['POST'])
+def create_smartphone():
+    data = request.get_json()
+
+    marca = data.get('Marca')
+    modello = data.get('Modello')
+    sistema_operativo = data.get('Sistema_operativo')
+    dimensioni_schermo = data.get('Dimensioni_schermo')
+    capacita_batteria = data.get('Capacita_batteria')
+
+    if not marca or not modello or not sistema_operativo or not dimensioni_schermo or not capacita_batteria:
+        return jsonify({"errore": "Tutti i campi sono obbligatori"}), 400
+
+    try:
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
+
+        query = "INSERT INTO Smartphones (Marca, Modello, Sistema_operativo, Dimensioni_schermo, Capacita_batteria) VALUES (%s, %s, %s, %s, %s)"
+        cursor.execute(query, (marca, modello, sistema_operativo, dimensioni_schermo, capacita_batteria))
+        conn.commit()
+
+        return jsonify({"successo": "Nuovo smartphone creato", "id": cursor.lastrowid}), 201
+    
+    except Error as e:
+        print("Errore nella connessione al database:", e)
+        return jsonify({"errore": "Impossibile inserire lo smartphone nel database"}), 500
+    
+    finally:
+        if conn.is_connected():
+            cursor.close()
+            conn.close()
 if __name__ == "__main__":
     app.run()
